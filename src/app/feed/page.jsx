1d9778f64165b94c832d96c3af5702c2a6df8e51
naiemhosen430 <naiemhosen430@gmail.com>
2023-11-05 07:58:22 -0800

    d

--- src/app/feed/page.jsx
+++ src/app/feed/page.jsx
@@ -8,7 +8,7 @@ export default function page() {
       <div className="space-y-4 p-2">
         <div className="rounded-full bg-slate-900 flex items-center justify-center sticky top-1">
           <input
-            className="text-xl p-2 px-4 rounded-full bg-slate-900 text-teal-50 w-10/12"
+            className="lg:text-xl text-sm p-2 px-4 rounded-full bg-slate-900 text-teal-50 w-10/12"
             type="search"
             name="text"
             id="text"
