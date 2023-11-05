36fa79b770f8b4fa2130db74fa5ad2551d916263
naiemhosen430 <naiemhosen430@gmail.com>
2023-11-05 06:59:04 -0800

    d

--- src/app/Components/Shared/Menu.jsx
+++ src/app/Components/Shared/Menu.jsx
@@ -1,9 +1,21 @@
+"use client";
 import Link from "next/link";
 import { AiOutlineMessage } from "react-icons/ai";
 import { MdOutlineNotificationsNone } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import { useState } from "react";
 
 export default function Menu() {
+  const [mobileBox, setMobileBox] = useState(false);
+
+  // onclick functions
+  const toggleMenu = () => {
+    if (mobileBox === true) {
+      setMobileBox(false);
+    } else {
+      setMobileBox(true);
+    }
+  };
   return (
     <>
       <div className="bg-slate-900 flex items-center p-4 px-4 shadow-md">
@@ -42,17 +54,60 @@ export default function Menu() {
         </ul>
 
         <ul className="lg:w-3/12 w-2/12 flex justify-end items-center space-x-4">
-          <Link
-            className="text-slate-500 text-2xl space-x-2 flex items-center justify-center hover:text-white px-4 py-2 rounded-md"
-            href={"/"}
+          <h1
+            className="text-slate-500 cursor-pointer text-2xl space-x-2 flex items-center justify-center hover:text-white px-4 py-2 rounded-md"
+            onClick={toggleMenu}
           >
             <span className="text-white lg:inline-block hidden">
               naiemhosen
             </span>
             <CgProfile />
-          </Link>
+          </h1>
         </ul>
       </div>
+
+      {mobileBox && (
+        <div className="lg:hidden w-screen h-screen bg-slate-900">
+          <ul className="space-y-2">
+            <Link
+              className="text-slate-500 hover:text-white px-4 py-2 block rounded-md"
+              onClick={toggleMenu}
+              href={"/"}
+            >
+              Person
+            </Link>
+
+            <Link
+              className="text-slate-500 hover:text-white px-4 py-2 block rounded-md"
+              onClick={toggleMenu}
+              href={"/"}
+            >
+              Video
+            </Link>
+            <Link
+              className="text-slate-500 hover:text-white px-4 py-2 block rounded-md"
+              onClick={toggleMenu}
+              href={"/"}
+            >
+              Profile
+            </Link>
+            <Link
+              className="text-slate-500 hover:text-white px-4 py-2 block rounded-md"
+              onClick={toggleMenu}
+              href={"/"}
+            >
+              Edit Profile
+            </Link>
+            <Link
+              className="text-slate-500 my-4 bg-slate-800 rounded-lg shadow-md hover:text-white px-4 py-2 block hover:bg-slate-500 text-center"
+              href={"/"}
+              onClick={toggleMenu}
+            >
+              Log out
+            </Link>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
