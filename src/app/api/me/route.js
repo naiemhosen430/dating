943cfc9fa7adf6f693315c4cc16d54d9c7062561
naiemhosen430 <naiemhosen430@gmail.com>
2023-11-07 20:48:54 -0800

    d

--- src/app/api/me/route.js
+++ src/app/api/me/route.js
@@ -2,18 +2,11 @@ import getDetaFromToken from "@/helper/getDetaFromToken";
 import User from "@/models/userModel";
 
 import { dbconnect } from "@/utils/mongo";
-import { NextResponse } from "next/server";
 
 export async function GET() {
   await dbconnect();
-  try {
-    const data = getDetaFromToken();
-    const result = await User.findOne({ email: data.email });
-    return NextResponse.json(
-      { statusCode: 200, message: "success", data: result },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.log(error);
-  }
+  const data = getDetaFromToken();
+  const result = await User.findOne({ email: data.email });
+
+  return Response.json({ statusCode: 200, message: "success", data: result });
 }
