6821aef06cf85aac8317abe704100b4a46f4db28
naiemhosen430 <naiemhosen430@gmail.com>
2023-11-10 03:34:21 -0800

    d

--- src/app/privecy-policy/page.jsx
+++ src/app/privecy-policy/page.jsx
@@ -1,5 +1,174 @@
+import Link from "next/link";
 import React from "react";
+import { CgArrowLeft } from "react-icons/cg";
+import { MdHelp } from "react-icons/md";
 
 export default function page() {
-  return <>page</>;
+  return (
+    <>
+      <div className="p-2 flex items-center text-3xl">
+        <div className="w-6/12">
+          <Link href={"/"}>
+            <CgArrowLeft />
+          </Link>
+        </div>
+        <div className="w-6/12 text-right">
+          <MdHelp className="inline-block" />
+        </div>
+      </div>
+      <div className="p-5 overflow-y-auto h-screen">
+        <h1 className="text-slate-500 text-center bg-slate-900 p-4 text-sm">
+          Last updated: 10 November 2023
+        </h1>
+        <h1 className="text-slate-500 text-lg p-4">
+          Welcome to Zane, a free chatting platform developed by MD Naiem Hosen.
+          We are committed to protecting your privacy and providing you with a
+          secure and enjoyable experience while using our application. This
+          Privacy Policy outlines the types of information we may collect, how
+          we use and protect that information, and your rights regarding your
+          personal data.
+        </h1>
+
+        <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+          Information We Collect
+        </h1>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            Account Information:
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            When you create an account on Zane, we collect your email, name, and
+            password to facilitate account management and authentication.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            User-Generated Content:
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            Any content, such as messages, photos, and videos, shared on Zane is
+            stored on our servers to enable communication between users.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">Log Data:</h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            We automatically collect information about your device and how you
+            use Zane, including IP addresses, device type, browser type, and
+            timestamps of actions.
+          </h1>
+        </div>
+
+        <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+          How We Use Your Information
+        </h1>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            Account Management:
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            Your account information is used for authentication, account
+            recovery, and to provide personalized features.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">Communication:</h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            We use your data to facilitate communication between users and
+            improve the user experience of Zane.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">Analytics:</h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            We may use aggregated and anonymized data for analytical purposes to
+            enhance and optimize Zane's performance.
+          </h1>
+        </div>
+
+        <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+          Data Sharing and Security
+        </h1>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            Third-Party Services:
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            We do not sell, trade, or share your personal information with third
+            parties for marketing purposes. However, we may use third-party
+            services for analytics and infrastructure purposes.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">Security:</h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            We implement industry-standard security measures to protect your
+            information from unauthorized access, disclosure, alteration, and
+            destruction.
+          </h1>
+        </div>
+
+        <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+          Your Choices and Rights
+        </h1>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            Account Settings:
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            You can review and update your account information in the settings
+            section of Zane.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            Communication Preferences:
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            You can control the types of notifications you receive through your
+            account settings.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">Data Deletion:</h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            You have the right to request the deletion of your account and
+            associated data. Contact us at [contact email] for assistance.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+            Changes to This Privacy Policy
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            We may update this Privacy Policy from time to time, and any changes
+            will be reflected on this page. Please review this policy
+            periodically for any updates.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+            Contact Us
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            If you have any questions, concerns, or requests regarding this
+            Privacy Policy, please contact us at naiemhosen430@gmail.com.
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/term-service/page.jsx
+++ src/app/term-service/page.jsx
@@ -1,5 +1,171 @@
+import Link from "next/link";
 import React from "react";
+import { CgArrowLeft } from "react-icons/cg";
+import { MdHelp } from "react-icons/md";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="p-2 flex items-center text-3xl">
+        <div className="w-6/12">
+          <Link href={"/"}>
+            <CgArrowLeft />
+          </Link>
+        </div>
+        <div className="w-6/12 text-right">
+          <MdHelp className="inline-block" />
+        </div>
+      </div>
+      <div className="p-5 overflow-y-auto h-screen">
+        <h1 className="text-slate-500 text-center bg-slate-900 p-4 text-sm">
+          Last updated: 10 November 2023
+        </h1>
+        <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+          Acceptance of Terms
+        </h1>
+        <h1 className="text-slate-500 text-lg p-4">
+          By accessing or using the Zane chatting platform ("Zane"), you agree
+          to comply with and be bound by the following Terms of Service. If you
+          do not agree with these terms, please do not use Zane.
+        </h1>
+
+        <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+          Use of Zane
+        </h1>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">Eligibility:</h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            You must be at least 13 years old to use Zane. If you are under 13,
+            please do not use the service.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            Account Creation:
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            To use certain features of Zane, you must create an account. You
+            agree to provide accurate and complete information during the
+            registration process and to update such information to keep it
+            accurate and current.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">User Conduct::</h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            You are responsible for your use of Zane, including the content you
+            create and share. You agree not to engage in any activity that may:
+          </h1>
+          <h1 className="text-slate-500 text-xs p-4">
+            Violate any laws, regulations, or third-party rights.
+          </h1>
+          <h1 className="text-slate-500 text-xs p-4">
+            Interfere with or disrupt Zane's operation.
+          </h1>
+          <h1 className="text-slate-500 text-xs p-4">
+            Harass, threaten, or harm other users.
+          </h1>
+        </div>
+
+        <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+          Content and Ownership
+        </h1>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            User-Generated Content:
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            By using Zane, you grant us a non-exclusive, transferable,
+            sub-licensable, royalty-free license to use, modify, and distribute
+            the content you create on Zane solely for the purpose of operating
+            and improving Zane.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            Intellectual Property::
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            Zane and its original content (excluding user-generated content) are
+            the property of MD Naiem Hosen and are protected by intellectual
+            property laws. You may not use Zane for any purpose not expressly
+            permitted by these terms.
+          </h1>
+        </div>
+
+        <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+          Privacy
+        </h1>
+
+        <div className="py-5">
+          <h1 className="text-2xl text-white font-bold p-2">
+            Data Collection:
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            Your use of Zane is subject to our Privacy Policy. By using Zane,
+            you consent to the collection and use of your information as
+            outlined in the Privacy Policy.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+            Termination
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            We reserve the right to terminate or suspend your account and access
+            to Zane for any reason, without notice, at our discretion.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+            Changes to Terms
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            We may update these Terms of Service from time to time. Any changes
+            will be posted on this page, and your continued use of Zane after
+            such changes will constitute your acceptance of the updated terms.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+            Disclaimer of Warranties
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            Zane is provided "as is" without any warranties, expressed or
+            implied. We do not guarantee that Zane will be uninterrupted,
+            secure, or error-free.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+            Limitation of Liability
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            To the fullest extent permitted by law, MD Naiem Hosen shall not be
+            liable for any indirect, incidental, special, consequential, or
+            punitive damages, or any loss of profits or revenues.
+          </h1>
+        </div>
+
+        <div className="py-5">
+          <h1 className="text-lg mt-10 text-center bg-slate-950 text-slate-500 font-bold p-2">
+            Contact Us
+          </h1>
+          <h1 className="text-slate-500 text-sm p-4">
+            If you have any questions or concerns about these Terms of Service,
+            please contact us at naiemhosen430@gmail.com.
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
