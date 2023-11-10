8ad575c1ae059dbeb6580171d1419a1ef548d3b2
naiemhosen430 <naiemhosen430@gmail.com>
2023-11-09 21:12:59 -0800

    d

--- src/app/api/people/[text]/route.js
+++ src/app/api/people/[text]/route.js
@@ -3,7 +3,10 @@ import { dbconnect } from "@/utils/mongo";
 
 export async function GET() {
   await dbconnect();
-  const users = await User.find().select(
+  const text = req.url.split("text/")[1];
+  const users = await User.find({
+    interest: { $regex: text, $options: "i" },
+  }).select(
     "-password -email -blocklist -friends -verificationcode -recent -rendom"
   );
   return Response.json({
