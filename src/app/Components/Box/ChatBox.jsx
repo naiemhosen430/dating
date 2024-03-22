3a00cfeeb4995534fdf34eac26c46882ca4edda2
naiemhosen430 <naiemhosen430@gmail.com>
2024-03-23 00:17:10 +0600

    d

--- src/app/Components/Box/ChatBox.jsx
+++ src/app/Components/Box/ChatBox.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import singlechatbox from "./singlechatbox";
 
 export default function ChatBox() {
   const [chats, setChats] = useState(null);
@@ -30,28 +31,9 @@ export default function ChatBox() {
       }
     };
     fetchData();
-  }, []);
+  }, [userInfo]); // Make sure to add userInfo as a dependency
 
-  useEffect(() => {
-    if (!chats || !userInfo) return;
-    const fetchDataForChats = async () => {
-      const promises = chats.map(async (chat) => {
-        const otherPersonId = chat.chatids.find((id) => id !== userInfo._id);
-        const userData = await axios.get(`/api/profile/${otherPersonId}`);
-        return userData.data.data;
-      });
-      Promise.all(promises)
-        .then((userDataArray) => {
-          const updatedChats = chats.map((chat, index) => ({
-            ...chat,
-            otherPersonData: userDataArray[index], // Add other person's data to each chat object
-          }));
-          setChats(updatedChats);
-        })
-        .catch((err) => console.log(err));
-    };
-    fetchDataForChats();
-  }, [chats, userInfo]);
+  console.log(chats);
 
   return (
     <>
@@ -66,43 +48,7 @@ export default function ChatBox() {
           {chats?.length === 0 ? (
             <h1 className="py-10 text-center">No chats found</h1>
           ) : (
-            chats?.map((chat) => (
-              <div
-                className="flex items-center justify-center p-2 px-1"
-                key={chat.id}
-              >
-                <div className="w-2/12 flex items-center rounded-full pb-1">
-                  <Link
-                    className="block"
-                    href={`/profile/${chat.otherPersonData?._id}`}
-                  >
-                    <img
-                      className="w-12 h-12 rounded-full inline-block"
-                      src={chat.otherPersonData?.profilepicture}
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="w-10/12">
-                  <Link href={`/converssion/${chat.otherPersonData?._id}`}>
-                    <h1 className="text-sm px-2">
-                      {chat.otherPersonData?.name}
-                    </h1>
-                    <h1 className="text-xs px-2 text-red-500">
-                      active 11 m ago
-                    </h1>
-                    <h1 className="text-xs px-2 text-red-400 text-right flex">
-                      <span className="w-8/12 text-left text-base block">
-                        Hello
-                      </span>
-                      <span className="text-xs text-right w-4/12 block text-red-500">
-                        5 m ago
-                      </span>
-                    </h1>
-                  </Link>
-                </div>
-              </div>
-            ))
+            chats?.map((chat) => <singlechatbox chat={chat} myid={userData?._id} />)
           )}
         </div>
       </div>
---
+++ src/app/Components/Box/singlechatbox.jsx
@@ -0,0 +1,46 @@
+"use client";
+import React, { useState } from "react";
+
+export default function singlechatbox({ chat , myid}) {
+  const [profileInfo, setProfileInfo] = useState(null);
+  const myfriendid = chat?.chatids.filter(item => item !== myid);
+  useEffect(() => {
+    const fatchData = async () => {
+      await axios
+        .get(`/api/profile/${myfriendid[0]}`)
+        .then((data) => {
+          setProfileInfo(data.data.data);
+        })
+        .catch((err) => {
+          setProfileInfo([]);
+          console.log(err);
+        });
+    };
+    fatchData();
+  }, []);
+  return (
+    <div className="flex items-center justify-center p-2 px-1" key={chat.id}>
+      <div className="w-2/12 flex items-center rounded-full pb-1">
+        <Link className="block" href={`/profile/${chat[0]?._id}`}>
+          <img
+            className="w-12 h-12 rounded-full inline-block"
+            src={profileInfo?.profilepicture}
+            alt=""
+          />
+        </Link>
+      </div>
+      <div className="w-10/12">
+        <Link href={`/converssion/${profileInfo?._id}`}>
+          <h1 className="text-sm px-2">{profileInfo?.name}</h1>
+          <h1 className="text-xs px-2 text-red-500">active 11 m ago</h1>
+          <h1 className="text-xs px-2 text-red-400 text-right flex">
+            <span className="w-8/12 text-left text-base block">Hello</span>
+            <span className="text-xs text-right w-4/12 block text-red-500">
+              5 m ago
+            </span>
+          </h1>
+        </Link>
+      </div>
+    </div>
+  );
+}
