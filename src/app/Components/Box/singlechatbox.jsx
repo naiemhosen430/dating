e817dc5e9a78119f1c3c2482614a0d591807b8fa
naiemhosen430 <naiemhosen430@gmail.com>
2024-03-23 16:57:51 +0600

    d

--- src/app/Components/Box/singlechatbox.jsx
+++ src/app/Components/Box/singlechatbox.jsx
@@ -1,5 +1,6 @@
 "use client";
 import axios from "axios";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 export default function Singlechatbox({ chat, myid }) {
