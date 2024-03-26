4eaded21f09e0bcbbb4aaabea5bbd5cae22f343a
naiemhosen430 <naiemhosen430@gmail.com>
2024-03-26 12:54:55 +0600

    d

--- src/app/Components/Box/ChatBox.jsx
+++ src/app/Components/Box/ChatBox.jsx
@@ -42,6 +42,41 @@ export default function ChatBox() {
           </Link>
         </h1>
         <div className="space-y-2">
+          <div className="flex items-center justify-center p-2 px-1">
+            <div className="w-2/12 flex items-center rounded-full pb-1">
+              <Link
+                className="block"
+                href={`/profile/65fd48a78af4b8a1e16a7b1d`}
+              >
+                <img
+                  className="w-12 h-12 rounded-full inline-block"
+                  src={
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCqu8BOhvKFDaFMAnjpAbK4o0VTGqo9BbeqTOvoWuVVfSqvqgG6hY5dc52EpEf5QTdBKY&usqp=CAU"
+                  }
+                  alt=""
+                />
+              </Link>
+            </div>
+            <div className="w-10/12">
+              <Link href={`/converssion/65fd48a78af4b8a1e16a7b1d`}>
+                <h1 className="text-sm px-2">
+                  MD Naiem
+                  <span className="px-2 text-xs text-slate-800">
+                    (Zane official)
+                  </span>
+                </h1>
+                <h1 className="text-xs px-2 text-red-500"></h1>
+                <h1 className="text-xs px-2 text-red-400 text-right flex">
+                  <span className="w-8/12 text-left text-base block">
+                    Hello, need hellp?
+                  </span>
+                  <span className="text-xs text-right w-4/12 block text-red-500">
+                    5 m ago
+                  </span>
+                </h1>
+              </Link>
+            </div>
+          </div>
           {chats?.length === 0 ? (
             <h1 className="py-10 text-center">No chats found</h1>
           ) : (
--- src/app/Components/Box/singlechatbox.jsx
+++ src/app/Components/Box/singlechatbox.jsx
@@ -103,41 +103,6 @@ export default function Singlechatbox({ chat, myid }) {
           </Link>
         </div>
       </div>
-
-      <div className="flex items-center justify-center p-2 px-1" key={chat.id}>
-        <div className="w-2/12 flex items-center rounded-full pb-1">
-          <Link className="block" href={`/profile/${profileInfo?._id}`}>
-            <img
-              className="w-12 h-12 rounded-full inline-block"
-              src={
-                profileInfo?.profilepicture
-                  ? profileInfo?.profilepicture
-                  : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCqu8BOhvKFDaFMAnjpAbK4o0VTGqo9BbeqTOvoWuVVfSqvqgG6hY5dc52EpEf5QTdBKY&usqp=CAU"
-              }
-              alt=""
-            />
-          </Link>
-        </div>
-        <div className="w-10/12">
-          <Link href={`/converssion/65fd48a78af4b8a1e16a7b1d`}>
-            <h1 className="text-sm px-2">
-              MD Naiem
-              <span className="px-2 text-xs text-slate-800">
-                (Zane official)
-              </span>
-            </h1>
-            <h1 className="text-xs px-2 text-red-500"></h1>
-            <h1 className="text-xs px-2 text-red-400 text-right flex">
-              <span className="w-8/12 text-left text-base block">
-                {lastMessage?.lastmessage}
-              </span>
-              <span className="text-xs text-right w-4/12 block text-red-500">
-                5 m ago
-              </span>
-            </h1>
-          </Link>
-        </div>
-      </div>
     </>
   );
 }
