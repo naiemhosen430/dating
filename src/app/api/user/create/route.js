8b1002ba711fbcaa3982c222c095aa9f763a9de4
naiemhosen430 <naiemhosen430@gmail.com>
2024-04-26 03:31:34 +0600

    d

--- src/app/api/user/create/route.js
+++ src/app/api/user/create/route.js
@@ -6,36 +6,88 @@ import { dbconnect } from "@/utils/mongo";
 export async function POST(NextRequest) {
   await dbconnect();
   try {
+    function generateRandomEmail(exampleWord) {
+      const domains = [
+        "gmail.com",
+        "yahoo.com",
+        "outlook.com",
+        "hotmail.com",
+        "aol.com",
+      ];
+      const timestamp = Date.now().toString().slice(-5);
+      const domain = domains[Math.floor(Math.random() * domains.length)];
+      const username = timestamp + exampleWord;
+
+      return username + "@" + domain;
+    }
+
+    const exampleWord = "example";
+    const exampleemail = generateRandomEmail(exampleWord);
+
     const data = await NextRequest.json();
 
     const checkUser = await User.findOne({ email: data.email }).select(
-      "password email _id email role"
+      "password email"
     );
 
     if (!checkUser || checkUser.password == "") {
+      const userObj = {
+        name: data.name,
+        email: data.email || exampleemail,
+        password: data.password,
+        age: data.age,
+        gender: data.gender,
+        country: data.country,
+        interest: data.interest,
+      };
+      const user = await User.create(userObj);
       return Response.json({
         statusCode: 404,
-        message: "Something wents wrong !!!",
+        id: user._id,
+        email: user.email,
+        message:
+          "You don't have account. Now set the password to create account",
       });
     }
 
-    const checkPassword = await bcrypt.compare(
-      data.password,
-      checkUser.password
+    return Response.json({
+      message: "You have an account. Now enter the password to continue",
+      email: checkUser.email,
+      statusCode: 200,
+    });
+  } catch (error) {}
+}
+
+export async function PUT(NextRequest) {
+  try {
+    await dbconnect();
+    const data = await NextRequest.json();
+
+    const hashPass = bcrypt.hashSync(data.password, 10);
+
+    await User.updateOne(
+      { email: data.email },
+      {
+        $set: {
+          name: data.name,
+          email: data.email,
+          password: hashPass,
+          age: data.age,
+          gender: data.gender,
+          country: data.country,
+          interest: data.interest,
+        },
+      }
     );
-
-    if (!checkPassword) {
-      return Response.json({
-        statusCode: 404,
-        message: "Wrong password!!! Try again",
-      });
-    }
+    const user = await User.findOne({ email: data.email }).select(
+      "_id role email"
+    );
 
     const secretKey = process.env.TOKEN_SECRET;
     const userData = {
-      userId: checkUser._id,
-      email: checkUser.email,
-      role: checkUser.role,
+      userId: user._id,
+      email: user.email,
+      role: user.role,
     };
     const expirationTimestamp =
       Math.floor(Date.now() / 1000) + 100 * 365 * 24 * 60 * 60;
@@ -46,8 +98,8 @@ export async function POST(NextRequest) {
 
     return Response.json({
       statusCode: 200,
-      token,
-      data: checkUser,
+      message: "success",
+      data: token,
     });
   } catch (error) {}
 }
