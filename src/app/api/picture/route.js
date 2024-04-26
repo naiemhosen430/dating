ba055287fa00790309e9c706c27b03af6a04a7b6
naiemhosen430 <naiemhosen430@gmail.com>
2024-04-26 12:06:59 +0600

    d

--- src/app/api/picture/route.js
+++ src/app/api/picture/route.js
@@ -6,7 +6,7 @@ export async function GET() {
     try {
   
       const data = await Picture.aggregate([
-        { $sample: { size: 1 } } 
+        { $sample: { size: 500 } } 
       ]);
       
 
