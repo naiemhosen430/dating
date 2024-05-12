a2d26348aefc510b1f62583d6df820f976b426c6
naiemhosen430 <naiemhosen430@gmail.com>
2024-05-12 18:48:49 +0600

    d

--- src/app/ntf/page.jsx
+++ src/app/ntf/page.jsx
@@ -16,7 +16,6 @@ export default function page() {
   
 useEffect(()=>{
   const fetchData = async () => {
-    setLoading(true);
     try {
       const response = await axios.get(`/api/ntf`);
       if (response?.data){
@@ -25,7 +24,6 @@ useEffect(()=>{
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
-      setLoading(false);
     }
   };
 
