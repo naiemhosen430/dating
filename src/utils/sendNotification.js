56551ab619c962312484bbe8282eef4f20ea9d02
naiemhosen430 <naiemhosen430@gmail.com>
2024-05-20 12:44:03 +0600

    d

--- src/utils/sendNotification.js
+++ src/utils/sendNotification.js
@@ -1,8 +1,6 @@
 
-  
-  function showNotification(data,message) {
+  export const showNotification = (title, body) => {
     if (window.Toaster) {
-      window.Toaster.postMessage(message);
+      window.Toaster.postMessage(JSON.stringify({ title, body }));
     }
-  }
-  
+  };
